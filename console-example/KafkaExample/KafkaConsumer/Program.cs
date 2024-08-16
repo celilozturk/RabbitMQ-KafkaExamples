8cbe9b04071f72500d3ef61ca45a646e5527052b
Celil Ozturk <celil.ozturk@windowslive.com>
2024-08-16 16:25:31 +0200

    Kafka console app example

---
+++ console-example/KafkaExample/KafkaConsumer/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Confluent.Kafka;
+using System.Threading;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var config = new ConsumerConfig
+        {
+            GroupId = "test-consumer-group",
+            BootstrapServers = "localhost:9092",
+            AutoOffsetReset = AutoOffsetReset.Earliest
+        };
+
+        using (var consumer = new ConsumerBuilder<Null, string>(config).Build())
+        {
+            consumer.Subscribe("test-topic");
+
+            CancellationTokenSource cts = new CancellationTokenSource();
+            Console.CancelKeyPress += (_, e) => {
+                e.Cancel = true;
+                cts.Cancel();
+            };
+
+            try
+            {
+                while (true)
+                {
+                    var consumeResult = consumer.Consume(cts.Token);
+                    Console.WriteLine($"Consumed message '{consumeResult.Value}' at: '{consumeResult.TopicPartitionOffset}'.");
+                }
+            }
+            catch (OperationCanceledException)
+            {
+                consumer.Close();
+            }
+        }
+    }
+}
---
+++ console-example/KafkaExample/KafkaProducer/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Confluent.Kafka;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var config = new ProducerConfig { BootstrapServers = "localhost:9092" };
+
+        using (var producer = new ProducerBuilder<Null, string>(config).Build())
+        {
+            try
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    var value = $"Message {i}";
+                    var result = await producer.ProduceAsync("test-topic", new Message<Null, string> { Value = value });
+                    Console.WriteLine($"Delivered '{result.Value}' to '{result.TopicPartitionOffset}'");
+                }
+            }
+            catch (ProduceException<Null, string> e)
+            {
+                Console.WriteLine($"Delivery failed: {e.Error.Reason}");
+            }
+        }
+    }
+}
